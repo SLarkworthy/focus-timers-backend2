34abd99b79b2fee6781b5906a4026544dfd005a3
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-08-11 18:13:38 -0400

    trying no domain again

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_focus-timer-app', domain: 'http://localhost:3000/'
+    Rails.application.config.session_store :cookie_store, key: '_focus-timer-app'
 else
-    Rails.application.config.session_store :cookie_store, key: '_focus-timer-app', domain: 'http://localhost:3000/'
+    Rails.application.config.session_store :cookie_store, key: '_focus-timer-app'
 end
