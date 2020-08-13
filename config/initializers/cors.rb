b7f57055bfc376df642162a483c9be9732b18ff6
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-08-13 15:07:31 -0400

    added frontend to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,12 +8,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         credentials: true
     end
 
-    # allow do
-    #     origins 'http://localhost:3000'
+    allow do
+        origins 'https://focusbuddy.herokuapp.com/'
       
-    #     resource '*',
-    #       headers: :any,
-    #       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-    #       credentials: true
-    # end
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head],
+          credentials: true
+    end
 end
